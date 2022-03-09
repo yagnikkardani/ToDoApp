385d92e1318f9e0a17b3ae9441126d19dc7b09ec
YK <kardaniyagnik@yagnik.com.au>
2022-03-09 20:39:22 +1000

    Remove Button

--- src/AddToList.js
+++ src/AddToList.js
@@ -1,8 +1,10 @@
+import { Button, Container, Stack} from 'react-bootstrap';
 import React, { useEffect, useState } from 'react';
+import { Form, ListGroup} from 'react-bootstrap';
 import styles from './components.module.css'
 
 function AddToList() {
-    const [name, setName] = useState("");
+    const [name, setName] = useState();
     const [toDoItems, setToDoItems] = useState([]);
     const [todolength, setToDoLength] = useState(false);
   
@@ -11,30 +13,41 @@ function AddToList() {
         setToDoItems([...toDoItems, name]);
     } 
     
+    const deleteTask = (i) => {
+        console.log(toDoItems[i])
+        setToDoItems(toDoItems.filter((element, index) => index !== i))
+    }
+
     useEffect(() => {
         toDoItems.length > 0 && setToDoLength(true);
     }, [toDoItems]);
 
     return(
-        <>
-        <div className={styles.inputStyle}>
-            <form onSubmit={handleSubmit}>
-                <label>Add Task:
-                    <input 
-                        type="text" 
-                        value={name} 
-                        onChange={(e) => setName(e.target.value)}
-                    />
-                </label>
-                <input type="submit"/>
-            </form>
+        <Container>
+        <Form onSubmit={handleSubmit} style={{marginTop: 50}}>
+            <Form.Group className="mb-3">
+                <Form.Label>Add Task</Form.Label>
+                <Form.Control 
+                    type='text' 
+                    placeholder='Enter Task' 
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+            </Form.Group>
+            <Button variant='primary' type='Submit'>Submit</Button>
+        </Form>
+        <ListGroup style={{marginTop: 10}}>
+            {todolength && <ListGroup.displayName>Tasks List</ListGroup.displayName>}
             
-            {todolength && <p>ToDo List: </p>}
-            
-            {toDoItems.map((i) => <p>{i}</p>)}
-        </div>
-        
-        </>
+                {toDoItems.map((i, index) => 
+                    <Stack direction="horizontal" gap={3}> 
+                        <ListGroup.Item className="mb">{i}</ListGroup.Item> 
+                        <div className="vr" />
+                        <Button variant="outline-danger" onClick={() => deleteTask(index)}>X</Button>
+                    </Stack>)}
+                
+        </ListGroup>
+        </Container>
     )
 }
 
