f9f6ddd093e9a92204e20979edc49285721732b6
YK <kardaniyagnik@yagnik.com.au>
2022-03-09 10:48:34 +1000

    First

---
+++ src/AddToList.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import styles from './components.module.css'
+
+function AddToList() {
+    const [name, setName] = useState("");
+    const [toDoItems, setToDoItems] = useState([]);
+    const [todolength, setToDoLength] = useState(false);
+  
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setToDoItems([...toDoItems, name]);
+    } 
+    
+    useEffect(() => {
+        toDoItems.length > 0 && setToDoLength(true);
+    }, [toDoItems]);
+
+    return(
+        <>
+        <div className={styles.inputStyle}>
+            <form onSubmit={handleSubmit}>
+                <label>Add Task:
+                    <input 
+                        type="text" 
+                        value={name} 
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                </label>
+                <input type="submit"/>
+            </form>
+            
+            {todolength && <p>Your ToDo List: </p>}
+            
+            {toDoItems.map((i) => <p>{i}</p>)}
+        </div>
+        
+        </>
+    )
+}
+
+export default AddToList;
