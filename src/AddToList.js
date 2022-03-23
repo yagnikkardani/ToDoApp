4aa0d784bb4e1bb3d91975b4867b42d81fcd8619
YK <kardaniyagnik@yagnik.com.au>
2022-03-23 12:27:08 +1000

    Including Docker

--- src/AddToList.js
+++ src/AddToList.js
@@ -1,54 +1,91 @@
-import { Button, Container, Stack} from 'react-bootstrap';
-import React, { useEffect, useState } from 'react';
-import { Form, ListGroup} from 'react-bootstrap';
-import styles from './components.module.css'
+import { Button, Container, Stack } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Form, ListGroup } from "react-bootstrap";
+import axios from "axios";
+import styles from "./components.module.css";
+
+const baseURL = "http://localhost:8000/apitodos/";
 
 function AddToList() {
-    const [name, setName] = useState();
-    const [toDoItems, setToDoItems] = useState([]);
-    const [todolength, setToDoLength] = useState(false);
-  
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setToDoItems([...toDoItems, name]);
-    } 
-    
-    const deleteTask = (i) => {
-        console.log(toDoItems[i])
-        setToDoItems(toDoItems.filter((element, index) => index !== i))
+  const [name, setName] = useState();
+  const [toDoItems, setToDoItems] = useState([
+    { id: "", todoitem: "", completed: false },
+  ]);
+  const [todolength, setToDoLength] = useState(false);
+  const [isDeleted, setIsDeleted] = useState(false);
+
+  const getAnswer = async () => {
+    const { data } = await axios(baseURL);
+    setToDoItems(data.map((i) => i));
+  };
+
+  useEffect(() => {
+    getAnswer();
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post(baseURL, { todoitem: name, completed: false })
+      .then((response) => {
+        setToDoItems([...toDoItems, response.data]);
+      });
+  };
+
+  const deleteTask = (i) => {
+    axios
+      .put(`${baseURL}${i.id}/`, { todoitem: i.todoitem, completed: true })
+      .then(() => {
+        setIsDeleted(true);
+      });
+  };
+
+  useEffect(() => {
+    if (isDeleted) {
+        getAnswer()
+        setIsDeleted(false)
     }
+  }, [isDeleted]);
+  useEffect(() => {
+    toDoItems.length > 0 && setToDoLength(true);
+  }, [toDoItems]);
 
-    useEffect(() => {
-        toDoItems.length > 0 && setToDoLength(true);
-    }, [toDoItems]);
+  return (
+    <Container>
+      <Form onSubmit={handleSubmit} style={{ marginTop: 50 }}>
+        <Form.Group className="mb-3">
+          <Form.Label>Add Task</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter Task"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </Form.Group>
+        <Button variant="primary" type="Submit">
+          Submit
+        </Button>
+      </Form>
 
-    return(
-        <Container>
-        <Form onSubmit={handleSubmit} style={{marginTop: 50}}>
-            <Form.Group className="mb-3">
-                <Form.Label>Add Task</Form.Label>
-                <Form.Control 
-                    type='text' 
-                    placeholder='Enter Task' 
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                />
-            </Form.Group>
-            <Button variant='primary' type='Submit'>Submit</Button>
-        </Form>
-        <ListGroup style={{marginTop: 10}}>
-            {todolength && <ListGroup.displayName>Tasks List</ListGroup.displayName>}
-            
-                {toDoItems.map((i, index) => 
-                    <Stack direction="horizontal" gap={3}> 
-                        <ListGroup.Item className="mb">{i}</ListGroup.Item> 
-                        <div className="vr" />
-                        <Button variant="outline-danger" onClick={() => deleteTask(index)}>X</Button>
-                    </Stack>)}
-                
-        </ListGroup>
-        </Container>
-    )
+      <ListGroup style={{ marginTop: 10 }}>
+        {todolength && (
+          <ListGroup.displayName>Tasks List</ListGroup.displayName>
+        )}
+
+        {toDoItems
+          .filter((e) => e.completed === false)
+          .map((i) => (
+            <Stack direction="horizontal" gap={3}>
+              <ListGroup.Item className="mb"> {i.todoitem} </ListGroup.Item>
+              <div className="vr" />
+              <Button variant="outline-danger" onClick={() => deleteTask(i)}>
+                X
+              </Button>
+            </Stack>
+          ))}
+      </ListGroup>
+    </Container>
+  );
 }
 
 export default AddToList;
